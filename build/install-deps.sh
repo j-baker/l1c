1be49bd4c0db4217282e65a8cf09384613e1e59d
James Baker <j.baker@outlook.com>
2015-01-31 18:12:47 +0000

    Build poly into polymer and reenable caching

--- build/install-deps.sh
+++ build/install-deps.sh
@@ -15,14 +15,11 @@ fi
 
 svn checkout --quiet svn://svn.code.sf.net/p/polyml/code/trunk polyml
 pushd polyml/polyml
-./configure --prefix=$HOME --enable-shared
+./configure --prefix=$HOME/polyml --enable-shared
 make
 make compiler
 make install
 popd
-
-export PATH=$PATH:$HOME/bin
-export LD_LIBRARY_PATH=$HOME/lib
 
 # HOL
 
